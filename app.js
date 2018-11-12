60b4761c0488db9cfd469f78fcc48d6a2114754b
Shweta Agarwal <idshweta@gmail.com>
2018-11-12 15:19:36 +0100

    changes in two files

--- app.js
+++ app.js
@@ -1,62 +1,54 @@
 
-//---    
 function searchInfo() {
     //const proxyurl = "https://cors-anywhere.herokuapp.com/"; // Use a proxy to avoid CORS error
     const api_key = "5B6D94AC6AFEB06A2F577D1DD406F143";
     var barcode = document.getElementById("barcode").value;
-    //var search = document.getElementById("search").value;
-    /*
-        var proxyurl = "https://api.barcodelookup.com/v2/products?";
-        if (barcode == "" && search == "") {
-            alert("please enter a search criteria");
-        } else if (!barcode == "") {
-            proxyurl += "barcode=" + barcode;
-            proxyurl += "&formatted=y&key=" + api_key;
-        } else if (!search == "") {
-            proxyurl += "search=" + search;
-            proxyurl += "&formatted=y&key=" + api_key;
-        } else if (!barcode == "" && !search == "") {
-            proxyurl += "barcode=" + barcode + "&search=" + search;
-            proxyurl += "&formatted=y&key=" + api_key;
-        }*/
-    
-    const url = "https://api.upcdatabase.org/product/"+ barcode + "/" + api_key;
-    //const url = "https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg";
-    //const url = "http://api.openweathermap.org/data/2.5/weather?q=tokyo&APPID=a6c98c5f1512ef700db17185202188c4&units=metric"
-    $.getJSON(url, codeData);
-    
-    //function to fetch information from json data to display
-    function codeData(data) {
-        alert(data);
-        document.getElementsByClassName("barcode_number").innerHTML = "Barcode-number = " + data.barcode_number;
-        document.getElementsByClassName("product_name").innerHTML = "Product-name = " + data.product_name;
-        document.getElementsByClassName("category").innerHTML = "Category =" + data.category;
-        document.getElementsByClassName("manufacturer").innerHTML = "Manufacturer = " + data.manufacturer;
-        document.getElementsByClassName("brand").innerHTML = "Brand-number = " + data.brand;
-        document.getElementsByClassName("nutrition_facts").innerHTML = "Nutrition-facts = " + data.nutrition_facts;
-        document.getElementsByClassName("ingredients").innerHTML = "Ingredients =" + data.ingredients;
-        document.getElementsByClassName("color").innerHTML = "Color =" + data.color;
-        document.getElementsByClassName("description").innerHTML = "Description =" + data.description;
-        document.getElementsByClassName("image").innerHTML = "Image =" + data.images;
-        document.getElementsByClassName("stores").innerHTML = "Stores = " + data.stores;
-        document.getElementsByClassName("reviews").innerHTML = "Reviews = " + data.reviews;
-        //console.log(data); 
-    };
+    var url = "https://api.upcdatabase.org/product/" + barcode + "/" + api_key;
+    // var url ='https://api.upcdatabase.org/product/888464220178/5B6D94AC6AFEB06A2F577D1DD406F143';
+    $.getJSON(url, function (Data) {
+        codeData(Data);
+    })
 };
+//function to fetch information from json data to display
+function codeData(data) {
+    //console.log(data); 
+   
+  
+        var barCode = document.getElementsByClassName("barcode_number1")
+        barCode[0].innerHTML = "<h3>Barcode-number = </h3>" + data.upcnumber;
 
-//document.getElementsByClassName("result").innerHTML= entireResponse();
-               //p5 screen//
-       /* var canvas;
-        function setup(){
-         canvas=createCanvas(windowWidth,windowHeight);
-         canvas.parent('canvasHolder');    
-       };
-       function mouseMoved(){
-           clear();
-       }; 
-       function draw(){
-           //background(175);
-           if (mouseMoved){
-           fill(200,250,200);
-           rect(mouseX,mouseY,30,30)};
-       };*/
+        var prodName = document.getElementsByClassName("product_name")
+        prodName[0].innerHTML = "<h3>Product-name = </h3>" + data.product_name;
+
+        var category = document.getElementsByClassName("category")
+        category[0].textContent = "<h3>Category =</h3>" + data.category;
+
+        var manufact = document.getElementsByClassName("manufacturer")
+        manufact[0].innerHTML = "<h3>Manufacturer = </h3>" + data.manufacturer;
+
+        var brand = document.getElementsByClassName("brand")
+        brand[0].innerHTML = "<h3>Brand-number = </h3>" + data.brand;
+
+        var nutri_fatcs = document.getElementsByClassName("nutrition_facts")
+        nutri_fatcs[0].innerHTML = "<h3>Nutrition-facts = </h3>" + data.nutrition_facts;
+
+        var ingre = document.getElementsByClassName("ingredients")
+        ingre[0].innerHTML = "<h3>Ingredients =</h3>" + data.ingredients;
+
+        var color = document.getElementsByClassName("color")
+        color[0].innerHTML = "<h3>Color =</h3>" + data.color;
+
+        var desc =document.getElementsByClassName("description")
+        desc[0].innerHTML = "<h3>Description =</h3>" + data.description;
+
+        var image = document.getElementsByClassName("image")
+        image[0].innerHTML = "<h3>Image =</h3>" + data.images;
+
+        var stores = document.getElementsByClassName("stores")
+        stores[0].innerHTML = "<h3>Stores = </h3>" + data.stores;
+
+        var reviews = document.getElementsByClassName("reviews")
+        reviews[0].innerHTML = "<h3>Reviews = </h3>" + data.reviews;
+       
+   
+};
